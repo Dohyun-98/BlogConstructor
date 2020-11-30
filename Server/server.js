4fb5a37e0cf5d10985bc5bc8ea9ab03dd05c5c23
Dohyun-Kim98 <ehgusrlaeh@naver.com>
2020-11-30 16:15:31 +0900

    DBConnect

--- Server/server.js
+++ Server/server.js
@@ -1,18 +1,27 @@
-const express = require('express')
-const path = require('path')
-const app = express()
-const port = 5000
-const ClientPath = path.join(__dirname,'../Client')
+'use strict';
+const express = require('express');
+const path = require('path');
+const app = express();
+const dbcon = require('../lib/dbconnection');
+const mysql = require('mysql');
+const port = process.env.port || 5000;
+const ClientPath = path.join(__dirname,'../Client');
+
+
+app.use(express.static(path.join(ClientPath,'/public')));
+
 
 app.get('/',(req,res)=>{
    
-    res.sendFile(path.join(__dirname,'../Client/index.html'))
+    res.sendFile(path.join(ClientPath,'/index.html'));
+    const title = dbcon('select title from Mydocument');
+    const contents = dbcon('select contents from Mydocument');
 })
 
-app.use(express.static(ClientPath))
+
 
 
 app.listen(port,function(err){
     if(err) console.log(err);
-    console.log(`listening on ${port}`)
+    console.log(`listening on ${port}`);
 })
