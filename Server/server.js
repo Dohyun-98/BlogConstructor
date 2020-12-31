de7655b8bce63a540d12f28e1bdc47cc73bb3b54
Dohyun-Kim98 <ehgusrlaeh@naver.com>
2020-12-31 15:41:26 +0900

    lg-version

--- Server/server.js
+++ Server/server.js
@@ -44,46 +44,97 @@ app.get('/', (req, res) => {
     });
 });
 
+app.get('/intro/introduce', (req, res) => {
+    if(req.session.displayName){
+        res.render(path.join(ClientPath, 'intro/lg-introduce.ejs'),{user:req.session.userName , pri:req.session.userPri });
+    }else{
+     res.render(path.join(ClientPath, 'intro/introduce.ejs'));
+    }
+});
+
 app.get('/member/myinfo', (req, res) => {
-    res.render(path.join(ClientPath, 'member/myinfo.ejs'))
+    if(req.session.displayName){
+        res.render(path.join(ClientPath, 'member/lg-myinfo.ejs'),{user:req.session.userName , pri:req.session.userPri });
+    }else{
+     res.render(path.join(ClientPath, 'member/myinfo.ejs'));
+    }
 });
 app.get('/member/sign-up-info', (req, res) => {
-    res.render(path.join(ClientPath, 'member/sign-up-info.ejs'))
+    if(req.session.displayName){
+        res.render(path.join(ClientPath, 'member/lg-sign-up-info.ejs'),{user:req.session.userName , pri:req.session.userPri });
+    }else{
+        res.render(path.join(ClientPath, 'member/sign-up-info.ejs'));
+    }
 });
 app.get('/notice/detail', (req, res) => {
-    res.render(path.join(ClientPath, 'notice/detail.ejs'))
+    if(req.session.displayName){
+        res.render(path.join(ClientPath, 'notice/lg-detail.ejs'),{user:req.session.userName , pri:req.session.userPri });
+    }else{
+        res.render(path.join(ClientPath, 'notice/detail.ejs'));
+    }
 });
 app.get('/notice/list', (req, res) => {
-    res.render(path.join(ClientPath, 'notice/list.ejs'))
+    if(req.session.displayName){
+        res.render(path.join(ClientPath, 'notice/lg-list.ejs'),{user:req.session.userName , pri:req.session.userPri });
+    }else{
+        res.render(path.join(ClientPath, 'notice/list.ejs'));
+    }
+    
 });
 app.get('/document/detail', (req, res) => {
-    res.render(path.join(ClientPath, 'document/detail.ejs'))
+    if(req.session.displayName){
+        res.render(path.join(ClientPath, 'document/lg-detail.ejs'),{user:req.session.userName , pri:req.session.userPri });
+    }else{
+        res.render(path.join(ClientPath, 'document/detail.ejs'));
+    }
+   
 });
 app.get('/document/list', (req, res) => {
-    res.render(path.join(ClientPath, 'document/list.ejs'))
+    if(req.session.displayName){
+        res.render(path.join(ClientPath, 'document/lg-list.ejs'),{user:req.session.userName , pri:req.session.userPri });
+    }else{
+        res.render(path.join(ClientPath, 'document/list.ejs'));
+    }
+    
 });
+app.get('/admin/notice-writer',(req,res)=>{
+    let Pri=req.session.userPri;
+    if(Pri=='admin'){
+    res.render(path.join(ClientPath, 'admin/notice-writer.ejs'),{user:req.session.userName , pri:req.session.userPri });
+    }else{
+        res.send('<script type="text/javascript">alert("권한이 없습니다.");history.back();</script>');
+        
+    }
+})
 app.post('/auth/login', (req, res) => {
     
-    const useremail = req.body.useremail;
-    const password = req.body.password;
-    const sql = 'select * from login where email=?';
-    const post = [useremail];
+    let useremail = req.body.useremail;
+    let password = req.body.password;
+    let sql = 'select * from login where email=?';
+    
 
-    connection.query(sql, post, (err, results, fields) => {
-        if (err) {
-            res.send('<script type="text/javascript">alert("아이디가 없습니다.");history.back();</script>');
-            console.log(err);
+    connection.query(sql, [useremail], function(error, results,fields){
+        if (error) {
+           //console.log(error);
+           res.send({
+            "code" : 400,
+            "failed": "error ocurred"
+        });
+        return;
         } else {
-            if (fields.length===0) {
+            if (!results[0]) {
                 res.send('<script type="text/javascript">alert("아이디가 없습니다.");history.back();</script>');
+                return;
             }
             if (results[0].password === password) {
                 req.session.displayName = useremail;
                 req.session.userName = results[0].name;
                 req.session.userPri = results[0].pri;
-                res.redirect('/');
+                res.send('<script type="text/javascript">location.href = document.referrer;</script>');
+                return;
             } else {
                 res.send('<script type="text/javascript">alert("패스워드 불일치");history.back();</script>');
+                return;
             }
         }
 
@@ -92,7 +143,8 @@ app.post('/auth/login', (req, res) => {
 
 app.get("/logout",(req,res)=>{
     delete req.session.displayName;
-    res.redirect('/');
+    delete req.session.userPri;
+    res.send('<script type="text/javascript">location.href = document.referrer;</script>');
 });
 
 app.listen(port, function (err) {
